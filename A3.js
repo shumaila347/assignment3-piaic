203283741bb6682a6fedbf5145d82cb5c50bb869
shumaila347 <56773348+shumaila347@users.noreply.github.com>
2019-11-29 18:46:20 +0500

    Add files via upload

---
+++ A3.js
@@ -0,0 +1,20 @@
+var A = prompt("Welcome to ABC Bekery. What do yo want to order sir/ma'am? \n1 Cake, Apple pie, Cookie, Chips, Patties");
+if
+(A == "cake"|| A == "CAKE" || A == "Cake"){
+    alert("cookies is available at index 0 in our bakery");
+}
+else if (A == "apple pie" || A == "APPLE PIE" || A == "Apple pie"){
+    alert("apple pie is availble at index 1 in our bakery");
+}
+else if (A == "cookie" || A == "COOKIE" || A == "Cookie"){
+    alert("cookies is available at index 2 in our bakery");
+}
+else if(A == "chips" || A == "CHIPS" || A == "Chips"){
+    alert("chips are available at index 3 in our bakery");
+}
+else if(A == "patties" || A == "PATTIES" || A == "Patties"){
+    alert("patties are available at index 4 in our bakery");
+}
+else if("");{
+alert("we are sorry, this item is not available in our bakery");
+}
